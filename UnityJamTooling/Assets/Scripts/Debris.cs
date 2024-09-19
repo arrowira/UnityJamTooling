3ac6e717ece8ff30e020bc3f371275a6c32cd72d
galaxyraider9wastaken <owendahms@gmail.com>
2024-09-18 23:10:10 -0700

    Added Pop

--- UnityJamTooling/Assets/Scripts/Debris.cs
+++ UnityJamTooling/Assets/Scripts/Debris.cs
@@ -7,6 +7,7 @@ public class Debris : MonoBehaviour
     private bool hasBeenEnlarged = false;
     public float speedMult = 1;
     private Rigidbody2D rb;
+    public AudioSource Pop;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,8 @@ public class Debris : MonoBehaviour
         float randMove = Random.Range(-3, 3);
         rb.AddTorque(10*randTurn);
         rb.AddForce(transform.right * 5 * randMove);
+        Pop = GetComponent<AudioSource>();
+        
     }
     public void Enlarge(float size)
     {
@@ -23,6 +26,7 @@ public class Debris : MonoBehaviour
             speedMult = 6;
             transform.localScale = new Vector3(transform.localScale.x * size, transform.localScale.y * size);
             hasBeenEnlarged = true;
+            Pop.Play();
         }
        
     }
