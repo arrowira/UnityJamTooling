c208f634f497cd1fbb28ed34de5df40e1c93f62e
arrowira <41136800+arrowira@users.noreply.github.com>
2024-09-18 17:43:54 -0700

    fuel bar

--- UnityJamTooling/Assets/Scripts/SMovement.cs
+++ UnityJamTooling/Assets/Scripts/SMovement.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-
+using UnityEngine.UI;
 using UnityEngine;
 
 public class SMovement : MonoBehaviour
@@ -12,6 +12,13 @@ public class SMovement : MonoBehaviour
     [SerializeField]
     private float speed = 2.0f;
     public bool isAccelerating = false;
+    private float fuel = 100f;
+    private float fuelConsumption = 0.1f;
+    [SerializeField]
+    private Image fuelBar;
+    [SerializeField]
+    private Image lowFuel;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -21,13 +28,29 @@ public class SMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.Space) && rb.velocity.magnitude < maxSpeed)
+        if (fuel < 15)
         {
-            rb.AddForce(transform.up * speed);
-            isAccelerating= true;
+            lowFuel.enabled = true;
         }
         else
         {
+            lowFuel.enabled = false;
+        }
+        fuelBar.fillAmount = fuel / 100;
+        if (Input.GetKey(KeyCode.Space) && rb.velocity.magnitude < maxSpeed && fuel > 0)
+        {
+            rb.AddForce(transform.up * speed);
+
+            fuel -= fuelConsumption;
+            isAccelerating= true;
+
+        }
+        else
+        {
+            if (fuel < 100){
+                fuel += fuelConsumption*1.7f;
+            }
+           
             isAccelerating= false;
         }
         
